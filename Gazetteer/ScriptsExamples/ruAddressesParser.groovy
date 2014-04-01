1e59ad02aae5ed8064963dc6985ee61384a14d8d
kiselev-dv <dmitry.v.kiselev@gmail.com>
2014-04-01 16:20:25 +0200

    sample script

---
+++ Gazetteer/ScriptsExamples/ruAddressesParser.groovy
@@ -0,0 +1,13 @@
+import me.osm.gazetter.addresses.AddressesParser;
+import me.osm.gazetter.addresses.impl.AddressesParserImpl;
+import org.json.*;
+
+class RuAddressesParser extends AddressesParserImpl implements AddressesParser {
+
+
+} 
+
+class AnotherClass {
+
+
+}
