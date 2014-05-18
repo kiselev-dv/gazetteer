e90bb79b11d2b509a950da1e317b1a6f9e535abb
kiselev-dv <dmitry.v.kiselev@gmail.com>
2014-05-18 12:20:20 +0200

    versioning

--- Gazetteer/src/main/java/me/osm/gazetter/Versions.java
+++ Gazetteer/src/main/java/me/osm/gazetter/Versions.java
@@ -1,7 +1,18 @@
 package me.osm.gazetter;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public class Versions {
-	public static final String gazetteer = "0.8.1";
-	public static final String dataFormat = "0.8";
-	public static final String osmDoc = "0.4";
+	
+	private static final Properties file = new Properties();
+	static { 
+		try {
+			file.load(Versions.class.getResourceAsStream("version.properties"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static final String gazetteer = file.getProperty("app.version");
 }
