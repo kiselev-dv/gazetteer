93a8342a3ce1efc182dc4a69101803541a481563
kiselev-dv <dmitry.v.kiselev@gmail.com>
2014-06-11 11:39:30 +0600

    fix err

--- GazetteerWeb/src/main/java/me/osm/gazetteer/web/api/InverseGeocodeAPI.java
+++ GazetteerWeb/src/main/java/me/osm/gazetteer/web/api/InverseGeocodeAPI.java
@@ -1,7 +1,17 @@
 package me.osm.gazetteer.web.api;
 
+import org.json.JSONObject;
+import org.restexpress.Request;
+import org.restexpress.Response;
+
 
 public class InverseGeocodeAPI {
-
+	
+	public JSONObject read(Request request, Response response){
+		
+		JSONObject result = new JSONObject();
+		
+		return result;
+	}
 
 }
