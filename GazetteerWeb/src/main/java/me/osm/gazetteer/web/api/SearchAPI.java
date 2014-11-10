8fb5d7bb29ad59d2b8d64ba5704fcc714dc4affe
kiselev-dv <dmitry.v.kiselev@gmail.com>
2014-11-10 19:26:47 +0500

    cleanup

--- GazetteerWeb/src/main/java/me/osm/gazetteer/web/api/SearchAPI.java
+++ GazetteerWeb/src/main/java/me/osm/gazetteer/web/api/SearchAPI.java
@@ -4,10 +4,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import me.osm.gazetteer.web.ESNodeHodel;
