8aba7b0318cbfa725df8794ddf641b7ec9b580e3
kiselev-dv <dmitry.v.kiselev@gmail.com>
2015-02-24 12:03:20 +0500

    snapshots generation

--- GazetteerWeb/config/templates/htmlRender.groovy
+++ GazetteerWeb/config/templates/htmlRender.groovy
@@ -2,6 +2,8 @@ import me.osm.gazetteer.web.api.imp.SnapshotRender;
 import groovy.text.SimpleTemplateEngine;
 import groovy.json.JsonSlurper;
 import me.osm.gazetteer.web.utils.OSMDocSinglton;
+import org.apache.commons.lang3.StringUtils;
+import me.osm.osmdoc.model.Tag.TagValueType;
 
 import java.util.Locale;
 
@@ -9,20 +11,48 @@ class HTMLSnapshotRender implements SnapshotRender {
 	
 	def engine = new SimpleTemplateEngine();
 	
-	def template = engine.createTemplate(new File('config/templates/feature.html'));
+	def tpls = [
+		'config/templates/feature.html',
+		'config/templates/poi-types.html',
+		'config/templates/poi-more-tags.html',
+		'config/templates/poi-related.html',
+		'config/templates/name.html',
+		'config/templates/address.html'
+	];
+
+	def osmdoc = OSMDocSinglton.get();
+	def locale = Locale.forLanguageTag("ru");
+
+	def translations = [
+		"templates.feature.showOnMap": "Показать на карте",
+		"templates.feature.poiTypes": "Направления деятельности:",
+		"templates.feature.address": "Адрес: ",
+		"templates.feature.building": "Здание",
+		"templates.feature.moreTags": "Дополнительная информация: ",
+		"templates.feature.sameBuilding": "Организации в том-же здании",
+		"templates.feature.sameType": "Организации того-же типа поблизости"
+	];
+	
+	def templates = [:];
+
+	public HTMLSnapshotRender() {
+		updateTemplate();
+	}
 	
 	@Override
 	public void updateTemplate() {
-		template = engine.createTemplate(new File('config/templates/feature.html'));
+		tpls.each { item ->
+			String[] s = StringUtils.split(item, "/");
+			String key = StringUtils.remove(s[s.length - 1], ".html"); 
+			
+			templates[key] = engine.createTemplate(new File(item));
+		}
 	}
-	
-	def osmdoc = OSMDocSinglton.get();
-	def locale = Locale.forLanguageTag("ru");
 	
 	@Override
 	public String render(String json) {
 		def jsonObj = new JsonSlurper().parseText(json);
-		return template.make([
+		return templates['feature'].make([
 			"f":jsonObj, 
 			"HTML_ROOT": "/", 
 			"RENDER":this, 
@@ -30,15 +60,6 @@ class HTMLSnapshotRender implements SnapshotRender {
 			"LOCALE": locale
 		]).toString();
 	}
-	
-	def translations = [
-		"templates.feature.showOnMap": "Показать на карте", 
-		"templates.feature.poiTypes": "Направления деятельности:",
-		"templates.feature.address": "Адрес: ",
-		"templates.feature.moreTags": "Дополнительная информация: ",
-		"templates.feature.sameBuilding": "Организации в том-же здании",
-		"templates.feature.sameType": "Организации тог-же типа поблизости"
-	];
 	
 	public String tr(String key) {
 		return translations[key] == null ? key : translations[key];
@@ -56,6 +77,17 @@ class HTMLSnapshotRender implements SnapshotRender {
 		}
 		
 		return r + "(" + types.substring(2) + ")";
+	}
+	
+	public String callTemplate(name, f, subj) {
+		return templates[name].make([
+			"f":f,
+			"subj": subj,
+			"HTML_ROOT": "/",
+			"RENDER":this,
+			"OSM_DOC": osmdoc,
+			"LOCALE": locale
+		]).toString();
 	}
 	
 }
