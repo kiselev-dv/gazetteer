dbf05a074721fed6ef092d4776713ee35e46a236
kiselev-dv <dmitry.v.kiselev@gmail.com>
2015-10-30 00:25:23 +0500

    logging for diff

--- Gazetteer/src/main/java/me/osm/gazetter/out/Diff.java
+++ Gazetteer/src/main/java/me/osm/gazetter/out/Diff.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeMap;
 
@@ -13,12 +14,18 @@ import me.osm.gazetter.utils.FileUtils;
 import me.osm.gazetter.utils.FileUtils.LineHandler;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Diff {
+	
+	private static final Logger log = LoggerFactory.getLogger(Diff.class); 
 	
 	private String oldPath;
 	private String newPath;
 	private PrintWriter out;
+	
+	private boolean fillOld = false;
 	
 	public Diff(String oldPath, String newPath, String out) {
 		try {
@@ -41,6 +48,8 @@ public class Diff {
 	
 	public void run() {
 		try {
+			log.info("Read {}", oldPath);
+			
 			FileUtils.handleLines(new File(this.oldPath), new LineHandler() {
 
 				@Override
@@ -53,6 +62,8 @@ public class Diff {
 				}
 				
 			});
+			
+			log.info("{} lines were readed", map.size());
 
 			final Set<String> olds = new HashSet<String>();
 			
@@ -89,15 +100,34 @@ public class Diff {
 				
 			});
 			
-			if(!map.isEmpty() || !olds.isEmpty()) {
+			// В мапе сейчас содержатся только те объекты 
+			// которых нет в --new
+			
+			// В olds сотержаться айдишки тех, кто есть в --old и они новее
+			// по идее таких быть не должно, кроме случая если перепутали
+			// --new и --old местами
+			
+			boolean fillDataForRemoved = !map.isEmpty() && fillOld;
+			boolean newer = !olds.isEmpty();
+			
+			if(newer) {
+				log.warn("There are objects in --old with newer timestamps");
+			}
+			
+			if(fillDataForRemoved || newer) {
 				FileUtils.handleLines(new File(this.oldPath), new LineHandler() {
-
+					
 					@Override
 					public void handle(String s) {
 						String id = GeoJsonWriter.getId(s);
 						
 						if(map.containsKey(id)) {
-							out.println("- " + s);
+							if(fillOld) {
+								out.println("- " + s);
+							}
+							else {
+								out.println("- " + "{\"id\":\"" + id + "\"}");
+							}
 						}
 						
 						else if(olds.contains(id)) {
@@ -106,6 +136,11 @@ public class Diff {
 					}
 					
 				});
+			}
+			else if (!fillOld) {
+				for(Entry<String, Object[]> entry : map.entrySet()) {
+					out.println("- " + "{\"id\":\"" + entry.getKey() + "\"}");
+				}
 			}
 			
 			out.flush();
