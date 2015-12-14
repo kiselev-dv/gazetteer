5e532b83542e8e026922d119d08b4331cad3cfc4
kiselev-dv <dmitry.v.kiselev@gmail.com>
2015-12-14 21:22:19 +0500

    DiffImport

---
+++ GazetteerWeb/src/main/java/me/osm/gazetteer/web/imp/LocationsDiffImporter.java
@@ -0,0 +1,10 @@
+package me.osm.gazetteer.web.imp;
+
+public class LocationsDiffImporter extends LocationsDumpImporter {
+
+	public LocationsDiffImporter(String source, boolean buildingsGeometry) {
+		super(source, buildingsGeometry);
+	}
+
+	
+}
